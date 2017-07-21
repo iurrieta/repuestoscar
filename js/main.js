a70d75494a23ebc41d0cf35e7913e426b5fc60ac
Ildemar <ildemar.92@gmail.com>
2017-07-21 16:56:30 -0400

    responsive + y retoques

--- js/main.js
+++ js/main.js
@@ -11,58 +11,13 @@ $(document).ready(function() {
             'margin-bottom': '0px'
         });
     }
- 
-	var owl = $("#header-slider");
 
-	owl.owlCarousel({
-		singleItem : true,
-		navigation : true,
-		navigationText : ["<i class='ion-ios-arrow-thin-left'></i>","<i class='ion-ios-arrow-thin-right'></i>"],
-		transitionStyle : "fade",
-		pagination: true,
-	});
 
-	$("#Client_Logo").owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-	    responsiveClass:true,
-	    responsive: {
-            0:{
-                items : 1
-            },
-            480:{
-                items : 1
-            },
-            768:{
-                items : 3
-            },
-            1200:{
-                items: 3
-            }
-        },
-        navigationText : ["<<",">>"],
-        pagination: true
-	});
-
-    $("#blog-post").owlCarousel({
-        autoPlay : 5000,
-        items : 3,
-        responsiveClass:true,
-        responsive: {
-            0:{
-                items : 1
-            },
-            480:{
-                items : 1
-            },
-            768:{
-                items : 3
-            },
-            1200:{
-                items: 3
-            }
-        }
+    $("#Client_Logo").owlCarousel({
+        autoPlay : 3000,
+        items : 5,
+        transitionStyle : "fade",
+        pagination: true,
     });
 
 
@@ -92,44 +47,6 @@ $(document).ready(function() {
         time: 2000
     });
 
-
-
-
-
-	  $('.carousel').carousel();
-
-
-    /**
-     * Google Map
-     */
-    if ( $('#googleMap').length ) {
-        var mapProp = {
-            center: new google.maps.LatLng(51.508742,-0.120850),
-            zoom:9,
-    scrollwheel: false,
-    navigationControl: false,
-    mapTypeControl: false,
-    scaleControl: false,
-    draggable: false,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById('googleMap'), mapProp);
-    }
-
-
-
-	/**
-     * Google Map
-     */
-    if ( $('#googleMap').length ) {
-        var mapProp = {
-            center: new google.maps.LatLng(41.878114, -87.629798),
-            zoom:9,
-            scrollwheel: false,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById('googleMap'), mapProp);
-    }
 
     // Animated Scrolling
     (function(){
